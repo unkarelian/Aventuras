90cff2281e1f8609bbbd185187cffab1c07f7534
Failerko <c.dann@seznam.cz>
2026-02-05 10:26:46 +0100

    chore: clear ded code

--- src/lib/services/database.ts
+++ src/lib/services/database.ts
@@ -1038,7 +1038,7 @@ class DatabaseService {
     await db.execute('DELETE FROM items WHERE story_id = ?', [storyId]);
     await db.execute('DELETE FROM story_beats WHERE story_id = ?', [storyId]);
 
-    // Restore entries (Not necessary as we are only deleting the last entry)
+    // Restore entries not necessary as we are only deleting the last entry
 
     // Restore characters
     for (const character of characters) {
